d21eaea29e6637c40e4b7b4cd548f9c3dd5eef31
peter6888 <qq1800@gmail.com>
2018-03-04 12:05:01 -0800

    change log location for run

--- src/run_cmd.sh
+++ src/run_cmd.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python run_summarization.py --mode=train --data_path=../data/finished_files/chunked/train_*.bin --vocab_path=../data/finished_files/vocab --log_root=~/log --exp_name=baseline
+python run_summarization.py --mode=train --data_path=../data/finished_files/chunked/train_*.bin --vocab_path=../data/finished_files/vocab --log_root=log --exp_name=dr_debug
