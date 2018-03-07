83a7d488119cbe79e852819a1c666723f5c81d15
Ubuntu <cs224n-team@cs224n-dev.glbkzlzj3y4exhbuwssnjm44uf.jx.internal.cloudapp.net>
2018-03-07 17:30:18 +0000

    batch for easy to print rouge score

---
+++ src/rouge.py
@@ -0,0 +1,10 @@
+import sys
+import os
+from decode import rouge_eval
+folder = sys.argv[1]
+#os.chdir(os.path.dirname(__file__))
+root = os.getcwd()
+print(os.path.join(root, folder))
+ref_dir = os.path.join(root, folder, 'reference')
+dec_dir = os.path.join(root, folder, 'decoded')
+print(rouge_eval(ref_dir, dec_dir))
